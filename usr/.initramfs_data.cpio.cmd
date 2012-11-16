141b92abea92b8b5f51baca94d6b3468628469d5
mmontuori <michael.montuori@gmail.com>
2012-11-16 01:18:46 -0500

    deleted unused files

--- usr/.initramfs_data.cpio.cmd
+++
@@ -1,1 +0,0 @@
-cmd_usr/initramfs_data.cpio := /bin/bash /home/michael/android/kernel/2.6.39.4/kernel/scripts/gen_initramfs_list.sh -o usr/initramfs_data.cpio   -d
--- usr/.initramfs_data.cpio.d
+++
@@ -1,1 +0,0 @@
-deps_initramfs := /home/michael/android/kernel/2.6.39.4/kernel/scripts/gen_initramfs_list.sh \
